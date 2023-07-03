cefa62a2381a81aff506480a6b63c177aa148b6f
Ryan Tomlinson <rtomlinson118@gmail.com>
2023-07-03 16:47:17 -0600

    Adds misc dir to sync

--- sync.example.sh
+++ sync.example.sh
@@ -6,5 +6,6 @@
 
 rsync ./env.sh user@1.2.3.4:~/project-directory
 rsync -rP --delete ./ansible user@1.2.3.4:~/project-directory
+rsync -rP --delete ./misc user@1.2.3.4:~/project-directory
 rsync -rP --delete ./packer user@1.2.3.4:~/project-directory
 rsync -rP --delete ./terraform user@1.2.3.4:~/project-directory

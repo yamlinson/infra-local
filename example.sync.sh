aafec858625bee0741cf169b8e939ac4263cac0d
yamlinson <95899054+yamlinson@users.noreply.github.com>
2024-07-21 15:24:52 -0600

    Update utility scripts

--- example.sync.sh
+++ example.sync.sh
@@ -2,7 +2,6 @@
 
 # EXAMPLE #
 # Just a quick script to sync this project from your dev machine to an infrastructure management machine 
-# Run this from VSCode with WSL
 
 rsync ./env.sh user@1.2.3.4:~/project-directory
 rsync -rP --delete ./ansible user@1.2.3.4:~/project-directory
